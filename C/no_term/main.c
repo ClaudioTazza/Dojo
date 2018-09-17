23fb7675568af4a86c41d12eb797e77872583925
claudiotazza <claudiotazza@gmail.com>
2018-09-17 07:52:27 +0200

    accorgimenti

--- C/no_term/main.c
+++ C/no_term/main.c
@@ -4,18 +4,22 @@ int main(){
   int addNum = 0, num = 0;
   FILE *f = fopen("sum.dat", "r");
 
-  printf("Lettura dal file sum.dat in corso ...");
-  fscanf(f, "%d", &num);
+  if(f){
+    printf("Lettura dal file sum.dat in corso ...");
+    fscanf(f, "%d", &num);
+  }
 
   printf("Valore corrente: %d\n\n", num);
 
   printf("Inserisci il valore da sommare: ");
   scanf("%d", &addNum);
 
-  fclose(f);
+  if(f) fclose(f);
   f = fopen("sum.dat", "w");
 
   fprintf(f, "%d", num + addNum);
+  printf("\nNuovo valore: %d\n"
+	"Salvataggio nel file sum.dat in conrso...\n", num + addNum);
 
   fclose(f);
   return 0;
