aeefb4d6e820d4e46d15abf15dbcd326a89bdcda
claudiotazza <claudiotazza@gmail.com>
2018-10-03 12:46:50 +0200

    Aggiunto massimo

---
+++ C/Massimo/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+  int numeri[10], maggiore = 0, i = 0;
+
+  printf("Inserisci 10 numeri, trovero' per te quello piu' grande\n");
+
+  while(i < 10){
+    printf("%d: ", i+1);
+    scanf("%d", &numeri[i]);
+
+    if(i == 0)
+      maggiore = numeri[i];
+    else if(numeri[i] > maggiore)
+      maggiore = numeri[i];
+
+    i += 1;
+  }/*Esegue il ciclo 10 volte*/
+
+  printf("Il maggiore e' %d\n", maggiore);
+
+  return 0;
+}
