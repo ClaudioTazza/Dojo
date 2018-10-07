96d778c62431a5831116a0bed5f48493623fbca9
claudiotazza <claudiotazza@gmail.com>
2018-10-07 21:36:24 +0200

    Cosi'?

--- C/Quadrato/main.c
+++ C/Quadrato/main.c
@@ -3,12 +3,12 @@
 int main(){
   int lato = 0, i = 0, j = 0;
   printf("Inserisci il lato (min 1, max 20): ");
-  scanf("%d", &lato);
 
-  while(lato < 0){
-    printf("Io un quadrato del genere non lo so disegnare.\n");
+  do{
+    if(lato < 0) printf("No.\n");
     scanf("%d", &lato);
-  }
+  }while(lato < 0);
+
   while(i < lato){ 
     while(j < lato){
       printf("*");
