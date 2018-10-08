32daa59d42d3322d5345427042bbcbc5b16faf88
claudiotazza <claudiotazza@gmail.com>
2018-10-08 12:05:56 +0200

    Accorgimenti

--- C/OccorenzaCifra/main.c
+++ C/OccorenzaCifra/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main(){
   int n = 0, conta = 0;
  
   do{
-    printf("Inserisci il numero: ");
+    printf("Inserisci il numero positivo e minore di %d: ", INT_MAX);
     scanf("%d", &n);
-  }while(n < 0);
+  }while(n < 0 || n > INT_MAX);
 
   do{
     if(n%10 == 7) conta++;
