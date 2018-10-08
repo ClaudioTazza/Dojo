e7025238a5127e366099d2f68d4d8dd4de084ea0
claudiotazza <claudiotazza@gmail.com>
2018-10-08 13:35:00 +0200

    Aggiustamenti

--- C/OccorenzaCifra/main.c
+++ C/OccorenzaCifra/main.c
@@ -7,7 +7,7 @@ int main(){
   do{
     printf("Inserisci il numero positivo e minore di %d: ", INT_MAX);
     scanf("%d", &n);
-  }while(n < 0 || n > INT_MAX);
+  }while(n < 0);
 
   do{
     if(n%10 == 7) conta++;
