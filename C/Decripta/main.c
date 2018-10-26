62c49974b97c0c2bd10171b0b85b4a221b0f65c5
claudio <claudiotazza@gmail.com>
2018-10-27 01:14:42 +0200

    Aggiunto Decripta

---
+++ C/Decripta/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+  int n, crit = 0, original;
+  int c1, c2, c3, c4; 
+
+  do{
+    printf("Inserisci un numero di quattro cifre: ");
+    scanf("%d", &n);
+  }while(n < 1000 || n > 9999);
+  
+  original = n;
+  
+  c4 = (((n%10) + 10) - 7) % 10; /*Modifica la quarta cifra*/
+  n /= 10;
+  c3 = (((n%10) + 10) - 7)% 10; /*Modifica la terza cifra*/
+  n /= 10;
+  c2 = (((n%10) + 10) - 7) % 10; /*Modifica la terza cifra*/
+  n /= 10;
+  c1 = (((n%10) + 10) - 7) % 10; /*Modifica la terza cifra*/
+  n /= 10;
+
+  crit = c3*1000 + c4*100 + c1*10 + c2;
+
+  printf("Numero da crittografare: %d\n", original);
+  printf("Numero crittografato: %d\n", crit);
+
+
+  return 0;
+}
