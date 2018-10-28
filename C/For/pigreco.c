190bdc47d9da57138451f29e3bd16272236afff9
claudio <claudio@lubuntu.lan>
2018-10-28 13:44:05 +0100

    Tentativo 1 semplificazione

--- C/For/pigreco.c
+++ C/For/pigreco.c
@@ -1,22 +1,25 @@
 #include<stdio.h>
 
 int main(){
-  int i, n, selector = 1;
+  int i, n, divisore = 3;
   double pi=4;
   
   printf("Approssimazione di Pi Greco\n"
 		  "mediante la serie:\n\n"
-		  "4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + ...\n\n"
-		  "Quanti termini vuoi utilizzare? ");
-  scanf("%d", &n);
+		  "4 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 + ...\n\n");
+  
+  do{
+	  printf("Quanti termini vuoi utilizzare? ");
+    scanf("%d", &n);
+  }while(n < 1);
 
-  for(i=3; i < (n*2) + 3; i+=2){
-    if(selector%2 == 1)
-      pi -= 4./i;
-    else
-      pi += 4./i;
+  for(i=0; i < n; i++){
+      pi -= 4./divisore;
+      divisore += 2;
 
-    selector += 1;
+      pi += 4./divisore;
+      divisore += 2;
+
   }  
 
   printf("PI: %f\n", pi);
