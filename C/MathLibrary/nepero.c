1b51024759399c72c4ced7a87b5ef2c863e47dc4
claudio <claudio@lubuntu.lan>
2018-10-30 21:44:22 +0100

    Ulteriori modifiche

--- C/MathLibrary/nepero.c
+++ C/MathLibrary/nepero.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-  double e = 1., tcd_e=1, prtcd_e=0;
+  double e = 1., ePrec=0;
   int i, fatt = 1;
 
   for(i=1; i <= 10; i++){
@@ -14,13 +14,12 @@ int main(){
   e = 1.;
   fatt = 1;
 
-  for(i=1; tcd_e > prtcd_e; i++){
-    prtcd_e = e + 0.001;
+  for(i=1; e > ePrec; i++){
+    ePrec = e + 0.001;
 
     e += 1./fatt;
     fatt *= i+1;
     
-    tcd_e = e;
   }
 
   printf("Numero di nepero con %d termini: %f\n", i, e); 
