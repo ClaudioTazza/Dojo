1c2ef45a11d3760a66db294202ca93369594c1aa
claudiotazza <claudiotazza@gmail.com>
2018-11-09 04:35:57 +0100

    Adesso il programma calcola anche l'ottale

--- C/Basi/basi.c
+++ C/Basi/basi.c
@@ -2,6 +2,7 @@
 
 int main(){
   int i, i2, temp_i;
+  int ott3bit, ott2bit;
 
   printf("N\t  Bin\t  Oct\t  Hex\n");
 
@@ -10,7 +11,8 @@ int main(){
     printf("%d\t", i);            /* Stampa il numero della riga*/
   
     temp_i = i;
-    
+   
+    /* Inizio Stampa binario */
     for(i2 = 128; i2 > 0 ; i2/= 2){
       if(temp_i - i2 > -1){
         putchar('1');
@@ -18,7 +20,29 @@ int main(){
       } 
       else
         putchar('0');
-    }/* Calcola e stampa il valore binario della riga*/
+    }
+    /* Fine stampa binario */
+  
+    printf("   ");
+
+    /* Inizio stampa ottale */
+    temp_i = i;
+    ott3bit = 0;
+    ott2bit = 0;
+
+    while(temp_i - 64 > -1){
+      ott3bit += 1;
+      temp_i -= 64;
+    }
+    printf("%d", ott3bit);
+
+    while(temp_i - 8 > -1){
+      ott2bit += 1;
+      temp_i -= 8;
+    }
+    printf("%d", ott2bit);
+    printf("%d", temp_i);
+    /* Fine stampa ottale */ 
 
     printf("\n");
   }
