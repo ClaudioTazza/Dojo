fb8ac38576149214824e6bb94add6985f53d8bfc
claudiotazza <claudiotazza@gmail.com>
2018-11-12 10:39:58 +0100

    aggiunto contavocali.c

---
+++ C/Caratteri/contavocali.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+  char c;
+  int conta = 0;
+
+  while((c = getchar()) != EOF){
+    switch(c){
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u': 
+      case 'A':
+      case 'E':
+      case 'I':
+      case 'O':
+      case 'U':
+        conta++;
+        break;
+      
+      default:
+        ;
+    }
+        
+  }
+
+  printf("Le volali presenti nel file sono %d\n\n", conta);
+
+  return 0;
+}
