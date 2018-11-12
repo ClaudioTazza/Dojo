cb04e1725a42813e58654c3447680954acbaaaba
claudiotazza <claudiotazza@gmail.com>
2018-11-12 10:24:07 +0100

    Aggiunto contaspaziature.c

--- C/Caratteri/contaspaziature.c
+++ C/Caratteri/contaspaziature.c
@@ -2,6 +2,14 @@
 
 int main(){
   int conta;
-  
+  char c;
+
+  while((c = getchar()) != EOF){
+    if(c == ' ')
+      conta++;
+  }
+
+  printf("Il numero di spazi e' di %d\n\n", conta);
+
   return 0;
 }
