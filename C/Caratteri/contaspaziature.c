a61f3368abfeac5ab4c06f92dbb1668c8725cf1a
claudiotazza <claudiotazza@gmail.com>
2018-11-12 11:39:14 +0100

    Modificato contaspaziature.c

--- C/Caratteri/contaspaziature.c
+++ C/Caratteri/contaspaziature.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
 int main(){
-  int conta, c;
+  int spazi = 0, tabulazioni = 0, newline = 0, c;
 
   while((c = getchar()) != EOF){
     switch(c){ 
       case '\n':
-      case '\t':
-      case ' ':
-        conta++;
+        newline++;
         break;
+
+      case '\t':
+        tabulazioni++;
+        break;
+
+      case ' ':
+        spazi++;
+        break;
+
       default:
         ;
     }
   }
 
-  printf("Il numero di spazi e' di %d\n\n", conta);
+  printf("Il numero di spazi e' di %d\n"
+         "Il numero di tabulazioni e' di %d\n"
+         "Il numero di newline e' di %d\n", spazi, tabulazioni, newline);
 
   return 0;
 }
