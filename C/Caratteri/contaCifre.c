376f806edf2ac9524678ac48ab5058514252715a
claudiotazza <claudiotazza@gmail.com>
2018-11-12 11:49:31 +0100

    Modificato contaCifre.c

--- C/Caratteri/contaCifre.c
+++ C/Caratteri/contaCifre.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-  int c, conta = 0;
+  int c, cifre = 0, spaziature = 0, altri = 0;
 
   while((c = getchar()) != EOF){
     switch(c){
@@ -15,16 +15,24 @@ int main(){
       case '8':
       case '9':
       case '0':
-        conta++;
+        cifre++;
         break;
-      
+
+      case ' ':
+      case '\t':
+      case '\n':
+        spaziature++;
+        break;
+
       default:
-        ;
+        altri++;
     }
         
   }
 
-  printf("Il numero delle cifre nel file sono %d\n\n", conta);
+  printf("Il numero delle cifre: %d\n"
+         "Il numero di spaziature: %d\n"
+         "Il numero dei caratteri restanti: %d\n", cifre, spaziature, altri);
 
   return 0;
 }
