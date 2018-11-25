25db98ca080c5a07bd1d95f9676afd3cbb6c8eff
claudiotazza <claudiotazza@gmail.com>
2018-11-25 18:44:56 +0100

    Modificato eliminacommenti.c

--- C/Caratteri/eliminacommenti.c
+++ C/Caratteri/eliminacommenti.c
@@ -3,8 +3,15 @@
 /* Stato          Input            Output           Next
  * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  * NORM          c == '/'            -             SLASH
- * NORM          c == '"'            c             STRINGA
+ * NORM          c == '\"'           c             STRINGA
+ * NORM          c == '\''           c             CARATTERE
  * NORM           else               c               = 
+ *
+ * CARATTERE     c == '\'            c             IGNORA
+ * CARATTERE     c == '\''           c             NORM 
+ * CARATTERE      else               c               =
+ *
+ * IGNORA        qualunque           c             CARATTERE
  *
  * STRINGA       c != '"'            c               = 
  * STRINGA        else               c             NORM  
@@ -12,7 +19,7 @@
  * SLASH         c != '*'         '/', c           NORM 
  * SLASH          else               -             COMMENTO
  *
- * COMMENTO      c != '*'            -               - 
+ * COMMENTO      c != '*'            -               = 
  * COMMENTO       else               -             ASTERISCO
  *
  * ASTERISCO     c != '/'            -             COMMENTO
@@ -22,22 +29,40 @@
 int main(){
   int c;
   int NORM = 0, SLASH = 1, COMMENTO = 2, ASTERISCO =  3, STRINGA = 4;
+  int CARATTERE = 5, IGNORA = 6;
   int STATO = NORM;
 
   while( (c = getchar()) != EOF){
     if(STATO == NORM){
       if(c == '/')
         STATO = SLASH;
-      else if(c == '"'){
+      else if(c == '\"'){
         STATO = STRINGA;
+        putchar(c);
+      }
+      else if(c == '\''){
+        STATO = CARATTERE;
         putchar(c);
       }
       else
         putchar(c);
     }
+
+    else if(STATO == CARATTERE){
+      if(c == '\\')
+        STATO = IGNORA;
+      else if(c == '\'')
+        STATO = NORM;
+      putchar(c);
+    }
+
+    else if(STATO == IGNORA){
+      putchar(c);
+      STATO = CARATTERE;
+    }
    
     else if(STATO == STRINGA){
-      if(c != '"')
+      if(c != '\"')
         putchar(c);
       else{
         putchar(c);
