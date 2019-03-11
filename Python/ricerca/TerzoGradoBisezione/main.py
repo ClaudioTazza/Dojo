cbc632f0d5b26d14a384bf3f7474cc5a0e393053
Claudio <claudio@claudio-PC.lan>
2019-03-11 22:05:39 +0100

    aggiustamenti

--- Python/ricerca/TerzoGradoBisezione/main.py
+++ Python/ricerca/TerzoGradoBisezione/main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 low = 0.3
 high = 0.75
 soglia = 0.00001
@@ -12,7 +14,7 @@ print "mediate metodo di bisezione\n\n"
 print "Intervallo di ricerca: (", low, ",", high, ")\n"
 print "Valore soglia per lo zero:", soglia, "\n\n"
 
-g = (low + high) // 2
+g = (low + high) / 2
 conta = 0 
 
 while low < high:
@@ -20,14 +22,14 @@ while low < high:
     lval = (low**3 + 4.5*low**2 + 3.5*low - 3)
     conta += 1
 
-    if gval < soglia:
+    if gval < 0:
         break
-    elif (gval < 0 and lval > 0) or (gval > 0 and lval < 0):
+    elif np.sign(gval) != np.sign(lval):
         high = g
     else:
         low = g
 
-    g = (low + high) // 2
+    g = (low + high) / 2
 
 print "Soluzione = ", g
 print "Passaggi = ", conta
