738fb3539bc68ce983c0585fc273379099341073
kuts <claudiotazza@gmail.com>
2019-06-06 09:12:31 +0200

    Aggiunto Conversioni2

---
+++ C/Funzioni/Conversioni2/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+void converti(int num, int base);
+
+int main(){
+  int decimale = 0, base = 0;
+
+  fputs("Decimale: ", stdout);
+  scanf("%d", &decimale);
+
+  fputs("Base: ", stdout);
+  scanf("%d", &base);
+
+  converti(decimale, base);
+  puts("\n");
+
+  return 0;
+}
+
+void converti(int num, int base){
+
+  int n = base;
+  int digit = 0;
+
+  if(base < 2 || base > 36){
+    puts("Base non appropriata");
+    return;
+  }
+  
+  while(n < 255){
+    n *= base;
+  }
+  
+  for(int bitVal = n/base; bitVal >= 1; bitVal/=base){
+
+    if(num >= bitVal){
+      digit = num/bitVal;
+      num %= bitVal;
+
+      if(digit < 9)
+        printf("%d", digit);
+      else
+	printf("%c", digit + 55);
+    }
+    else
+      printf("0");
+  }
+  
+}
