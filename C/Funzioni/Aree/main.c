1c4288b89547e72cb2bcc1e10593c27958c632f3
claudiotazza <claudiotazza@gmail.com>
2019-07-14 01:58:40 +0200

    Modifiche ad Aree

--- C/Funzioni/Aree/main.c
+++ C/Funzioni/Aree/main.c
@@ -1,26 +1,53 @@
 #include <stdio.h>
 
+void menu();
 double area_quadrato(double lato);
 double area_cerchio(double raggio);
 
 int main(){
-  int lato = 0, area_q = 0;
-  int raggio = 0, area_c = 0;
+  int scelta = 0;
+  double lato = 0., area_q = 0.;
+  double raggio = 0., area_c = 0.;
 
-  fputs("Inserisci lato del quadrato\n", stdout);
-  scanf("%d",&lato);
+  puts("Calcola l'area di figure geomentriche\n\n");
 
-  fputs("Inserisci il raggio del cerchio\n", stdout);
-  scanf("%d",&raggio);
+  menu();
+  scanf("%d", &scelta);
+  if(scelta < 1 || scelta > 2){
+    puts("Scelta non valida");
+    return 0;
+  }
+
+  puts("\n");
+
+  switch(scelta){
+    case 1:
+      puts("Inserisci lato del quadrato");
+      scanf("%lf",&lato);
+
+      area_q = area_quadrato(lato);
+      printf("Area quadrato = %lf\n", area_q);
+      break;
+
+    case 2:
+      puts("Inserisci il raggio del cerchio");
+      scanf("%lf",&raggio);
+
+      area_c = area_cerchio(raggio);
+      printf("Area cerchio= %lf\n", area_c);
+      break;
+
+    default:
+      puts("Qualcosa e' andato storto");
+  }
 
 
-  area_q = area_quadrato(lato);
-  area_c = area_cerchio(raggio);
-
-  printf("Area quadrato = %d\n", area_q);
-  printf("Area cerchio= %d\n", area_c);
 
   return 0;
+}
+
+void menu(){
+  fputs("Scegli la figura geometrica:\n1) Quadrato\n2) Cerchio\nScegli: ", stdout);
 }
 
 double area_quadrato(double lato){
