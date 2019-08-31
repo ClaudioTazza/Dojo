8c82225db80b6d46d00aee83a4878cf8d2cb3bc1
claudiotazza <claudiotazza@gmail.com>
2019-08-31 16:45:42 +0200

    Modifiche guess

--- C/Funzioni/Guess/main.c
+++ C/Funzioni/Guess/main.c
@@ -4,32 +4,40 @@
 int main(){
   int rispostaEsatta = 0;
   int tentativo = 0;
+  double again = 'n';
 
   guess_init();
 
-  rispostaEsatta = guess_getAnswer();
-
-  while(1){
-
-    putchar('\n');
-
+  do{
+    rispostaEsatta = guess_getAnswer();
     puts("I gave a number between 1 and 1000.");
     puts("Can you guess my number?");
     puts("Please type your first guess.");
     printf("(Risposta esatta = %d).\n", rispostaEsatta);
+ 
+    while(1){
+      putchar('\n');
 
-    scanf("%d", &tentativo);
+      scanf("%d", &tentativo);
 
-    putchar('\n');
+      putchar('\n');
 
-    if(tentativo < rispostaEsatta)
-      guess_low();
-    else if(tentativo > rispostaEsatta)
-      guess_high();
-    else{
-      guess_right();
-      break;
+      if(tentativo < rispostaEsatta)
+        guess_low();
+      else if(tentativo > rispostaEsatta)
+        guess_high();
+      else{
+        guess_right();
+        break;
+      }
     }
-  }
-  return 0;
+
+    /* Vuoi giocare ancora? */
+    puts("Would you like to play again (y or n)?");
+    do{
+      again = getchar();
+    }while(again != 'n' && again != 'y');
+
+  }while(again == 'y');
+    return 0;
 }
