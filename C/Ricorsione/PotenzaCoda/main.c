c34e1160c77f422aa44db9464023ab06e3232fcb
kuts <claudiotazza@gmail.com>
2019-09-04 19:59:24 +0200

    Aggiunto potenzaCoda

---
+++ C/Ricorsione/PotenzaCoda/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int potenza_helper(int, int, int);
+int potenza(int, int);
+
+int main(){
+  printf("%d\n", potenza(10, 3));
+
+  return 0;
+}
+
+int potenza_helper(int n1, int n2, int ris){
+  if(n2 == 0)
+    return ris;
+  else{
+    ris *= n1;
+    potenza_helper(n1, n2-1, ris);
+  } 
+}
+
+int potenza(int n1, int n2){
+  potenza_helper(n1, n2, 1);
+}
