43445330dadc31839b8e3bdaa45b7a3897e4870a
kuts <claudiotazza@gmail.com>
2019-09-04 20:56:03 +0200

    modifiche

--- Python/Ricorsione/PotenzaCoda/main.py
+++ Python/Ricorsione/PotenzaCoda/main.py
@@ -8,11 +8,7 @@ def potenzaCoda_Helper(base, esponente, ris):
   else:
     ris *= base
     esponente -= 1
-    print(base)
-    print(esponente)
-    print(ris)
-    potenzaCoda_Helper(base, esponente, ris)
+    return potenzaCoda_Helper(base, esponente, ris)
 
 
 print( potenzaCoda(10, 1) )
-print( potenzaCoda_Helper(10, 0, 10) )
