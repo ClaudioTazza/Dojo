51bb9ab2f47f82115b0d6fb0329d6eed7bc866e4
kuts <claudiotazza@gmail.com>
2019-09-15 09:12:30 +0200

    Modifiche

--- C/Funzioni/Memoizzazione/fibonacci.c
+++ C/Funzioni/Memoizzazione/fibonacci.c
@@ -8,22 +8,23 @@ void fibonacci_helper(int n, int cache[]){
     cache[n] = n;
   }
   else{
+    if(cache[n-1] == 0)
+      fibonacci_helper(n-1, cache);
+
     cache[n] = cache[n-1] + cache[n-2];
   }
 }
 
 int fibonacci(int n){
 
-  int vett[50] = { 0 };
+  static int vett[50] = { 0 };
 
-   
   if(n > 50 || n < 1){
     printf("Numero non valido");
     return -1;
   }
 
-  for(int i = 0; i < n; i++)
-    fibonacci_helper(i, vett);
+  fibonacci_helper(n, vett);
 
   return vett[n-1];
 }
